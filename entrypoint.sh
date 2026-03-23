e2a5e13cdbd014c6310cf687d5dc3247a3bd4292
huckchen0422 <99940187+huckchen0422@users.noreply.github.com>
2026-03-24 01:42:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ Latest Commit Log is ${PREFIX}_${COMMITLOG}
 IP address list are ${PREFIX}_${IPS}
 Datetime is $(date)
 
-Live-Demo for gitops
+Live-Demo for GitOpsv0.0.1
 EOF
 
 #Generate a new index.html under /var/www/html
